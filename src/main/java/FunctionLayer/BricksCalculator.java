28154ec557c2d7efc6ad56b31240b9025af2d197
Ionsight <ionsight@users.noreply.github.com>
2018-04-03 08:36:39 +0200

    Calculator stuff

---
+++ src/main/java/FunctionLayer/BricksCalculator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FunctionLayer;
+
+import java.util.List;
+
+/**
+ *
+ * @author Dradrach
+ */
+public class BricksCalculator {
+    
+    public static List<Brick> calculate(Order order){
+        return null;
+    }
+}
